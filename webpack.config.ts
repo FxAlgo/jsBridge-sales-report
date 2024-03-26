e7b475c4b09acfb6825ebba684cbc1b375bb686e
Marcel Saffa <marcel.saffa@gmail.com>
2024-03-26 16:40:21 +0100

    Debugging fix

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,56 +1,52 @@
-import path from "path";
 import HtmlWebpackPlugin from "html-webpack-plugin";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
-import CssMinimizerPlugin from "css-minimizer-webpack-plugin";
+import path from "path";
 
-const getConfig = (
-	env: { [key: string]: string },
-	argv: { [key: string]: string }
-): any => {
+const getConfig = (env: { [key: string]: string }, argv: { [key: string]: string }): any => {
 	return {
-		context: __dirname,
-		entry: path.resolve(__dirname, './src/index.tsx'),
+		context: path.resolve(__dirname, "src"),
+		entry: "./index.tsx",
 		module: {
 			rules: [
 				{
 					test: /\.(ts|tsx)?$/,
-					use: 'ts-loader',
+					use: "ts-loader",
 					exclude: /node_modules/,
 				},
 				{
 					test: /\.css$/,
-					use: [MiniCssExtractPlugin.loader, "css-loader"]
-				}
+					use: [MiniCssExtractPlugin.loader, "css-loader"],
+				},
 			],
 		},
 		mode: argv.mode === "production" ? "production" : "development",
 		resolve: {
-			extensions: ['.tsx', '.ts', '.js'],
+			extensions: [".tsx", ".ts", ".js"],
 		},
 		output: {
-			filename: 'index.js',
-			path: path.resolve(__dirname, 'dist'),
+			filename: "index.js",
+			path: path.resolve(__dirname, "dist"),
 		},
 		plugins: [
 			new HtmlWebpackPlugin({
-				title: 'JsBridge plugin',
-				template: 'src/index.html'
+				title: "JsBridge plugin",
+				template: "./index.html",
 			}),
 			new MiniCssExtractPlugin({
-				filename: "styles.css"
-			})
+				filename: "./styles.css",
+			}),
 		],
-		optimization: {
-			minimizer: [
-				new CssMinimizerPlugin()
-			],
-		},
+		//optimization: {
+		//	minimizer: [new CssMinimizerPlugin()],
+		//},
+		devtool: "source-map",
+
 		devServer: {
 			static: path.join(__dirname, "dist"),
 			compress: true,
-			port: 4000
+			port: 4000,
 		},
 	};
-}
+};
 
 export default getConfig;
